f16ed21732e65ccc86fbdd33f291cd03635f14b4
Jingyu Zhang <26292088+jyzhangr@users.noreply.github.com>
2017-09-10 16:43:43 +0800

    Create do_input.py

---
+++ Basic/do_input.py
@@ -0,0 +1,6 @@
+#The function input can be write as "input()", in this function, you can write something in parentheses, and the content
+#you write will be displayed.
+
+name=input("Please input your name:")
+# if you don't want to display anything, this code can be written as "name=input()"
+print("hello",name)
