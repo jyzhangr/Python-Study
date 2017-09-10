49872a995d8e8263a259a6e6cf2cc38dea4a15e2
Jingyu Zhang <26292088+jyzhangr@users.noreply.github.com>
2017-09-10 16:43:17 +0800

    Create do_print.py

---
+++ Basic/do_print.py
@@ -0,0 +1,6 @@
+#Sample 1
+print("hello world")  #in the parentheses, single quotes('') and double quotes("") are both allowed, but you can't begin with single
+                      #quote and end with double quote or begin with double quote and end with single quotes
+
+#Sample2
+print('hello','world')  #the results is the same as previous line code. in python, comma will be displayed as space.
