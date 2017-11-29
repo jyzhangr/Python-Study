a87bae2d93a510cd838c25bfaf4056c2fa3674ed
Jingyu Zhang <26292088+jyzhangr@users.noreply.github.com>
2017-11-29 15:27:27 +0800

    Create sorted.py

---
+++ Function/sorted.py
@@ -0,0 +1,13 @@
+
+# -*- coding: utf-8 -*-
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+def by_name(t):
+    return t[0]
+L2 = sorted(L, key=by_name)
+print(L2)
+
+def by_score(s):
+    return s[1]
+L3 = sorted(L, key=by_score)
+print(L3)
